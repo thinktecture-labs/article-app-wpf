84a980425e797e48afc1ce8da7309896bf33a292
Christian Liebel <christian.liebel@thinktecture.com>
2020-04-15 10:07:49 +0200

    refactor(models): remove DataContract/Member attributes

--- WpfApp1/Models/Article.cs
+++ WpfApp1/Models/Article.cs
@@ -2,31 +2,15 @@
 
 namespace WpfApp1.Models
 {
-    [DataContract]
     public class Article
     {
-        [DataMember]
         public int Id { get; set; }
-
-        [DataMember]
         public string Title { get; set; }
-
-        [DataMember]
         public string Manufacturer { get; set; }
-
-        [DataMember]
         public string Category { get; set; }
-
-        [DataMember]
         public string Subcategory { get; set; }
-
-        [DataMember]
         public double Price { get; set; }
-
-        [DataMember]
         public int? PreviousId { get; set; }
-
-        [DataMember]
         public int? NextId { get; set; }
     }
 }
--- WpfApp1/Models/Category.cs
+++ WpfApp1/Models/Category.cs
@@ -2,13 +2,9 @@
 
 namespace WpfApp1.Models
 {
-    [DataContract]
     public class Category
     {
-        [DataMember]
         public string Id { get; set; }
-
-        [DataMember]
         public string Key { get; set; }
     }
 }
